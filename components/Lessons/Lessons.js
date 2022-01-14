e9b3aad6ef889a8e0200120f41bd5b7da9a9caf2
alshakerM <82781870+alshakerM@users.noreply.github.com>
2022-01-14 22:59:06 +0100

    Add lessons page

--- components/Lessons/Lessons.js
+++ components/Lessons/Lessons.js
@@ -4,9 +4,17 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import medData from '../../public/med-data.json';
 import React from 'react';
 import cx from 'classnames';
+import {
+  Clear,
+  PlayArrowRounded,
+  PlayCircleOutline,
+  PlayCircleRounded,
+} from '@mui/icons-material';
+import { style } from '@mui/system';
 
 export function Lessons() {
-  const audioData = medData.map((data) => data.mp3);
+  const [activeAudio, setActiveAudio] = React.useState(undefined);
+  const openAudio = medData.find((med) => med.mp3 === activeAudio);
   const guide = medData.map((data) => data.instructor);
   const noDuplicatesGuide = guide.filter(function (value, index, array) {
     return array.indexOf(value) === index;
@@ -17,15 +25,22 @@ export function Lessons() {
   });
   const [isGuideDropped, setIsGuideDropped] = React.useState(false);
   const [isTypeDropped, setIsTypeDropped] = React.useState(false);
-  const [activeAudio, setActiveAudio] = React.useState(undefined);
-  console.log({ activeAudio });
+  const [isExpanded, setIsExpanded] = React.useState(false);
+  const [currentAudioTime, setCurrentAudioTime] = React.useState(0);
+  const [audioTimeLeft, setAudioTimeLeft] = React.useState(0);
+  const [currentProgress, setCurrentProgress] = React.useState(0);
+  const [pause, setPause] = React.useState(true);
+  const audioRef = React.useRef();
+  React.useEffect(() => {
+    if (pause) {
+      audioRef.current?.pause();
+    } else {
+      audioRef.current?.play();
+    }
+  }, [pause]);
   const mins = '< 5 minutes';
-  /* const expandAudio = React.useCallback(
-    function expandAudio() {
-      setIsExpanded(audioData);
-    },
-    [audioData, setIsExpanded]
-  ); */
+  if (activeAudio && isExpanded) {
+  }
   return (
     <div>
       <div className={styles.backgroundImgContainer}>
@@ -88,7 +103,10 @@ export function Lessons() {
             <div
               className={styles.audioStatusContainer}
               key={data.mp3}
-              onClick={() => setActiveAudio(data.mp3)}
+              onClick={() => {
+                setIsExpanded(true);
+                setActiveAudio(data.mp3);
+              }}
             >
               <div className={styles.songInfo}>
                 <p className={styles.songName}>{data.title}</p>
@@ -104,6 +122,62 @@ export function Lessons() {
           ))}
         </div>
       </div>
+      {activeAudio && isExpanded && (
+        <div className={styles.expandedContent}>
+          <div className={styles.expandedHeader}>
+            <p className={styles.expandedTitle}>LESSON</p>
+            <Clear
+              className={styles.expandedClearIcon}
+              onClick={() => setIsExpanded(false)}
+            />
+          </div>
+          <div className={styles.expandedBody}>
+            <p className={styles.expandedSongName}>{openAudio.title}</p>
+            <p className={styles.expandedSonAuthor}>{openAudio.instructor}</p>
+          </div>
+          <div className={styles.expandedAudioSection}>
+            <p>
+              {currentAudioTime < 60
+                ? `00:${
+                    currentAudioTime <= 10
+                      ? `0${currentAudioTime.toFixed(0)}`
+                      : currentAudioTime.toFixed(0)
+                  }`
+                : `0${(currentAudioTime / 60).toFixed(2)}`}
+            </p>
+
+            <button
+              className={styles.playButton}
+              onClick={() => setPause(!pause)}
+            >
+              <PlayArrowRounded className={styles.playIcon} fontSize="large" />
+            </button>
+            <p className={styles.expandedTimeLeft}>
+              {audioTimeLeft === 0
+                ? (openAudio.duration / 60).toFixed(2)
+                : `0${(audioTimeLeft / 60).toFixed(2)}`}
+            </p>
+            <audio
+              src={openAudio.mp3}
+              ref={audioRef}
+              onTimeUpdate={(event) => {
+                setAudioTimeLeft(event.target.duration);
+                setCurrentAudioTime(event.target.currentTime);
+                setCurrentProgress(
+                  event.target.currentTime / event.target.duration
+                );
+              }}
+              muted={false}
+            />
+          </div>
+          <div className={styles.progressBar}>
+            <div
+              className={styles.expandedProgressBar}
+              style={{ width: `${currentProgress * 100}%` }}
+            ></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
