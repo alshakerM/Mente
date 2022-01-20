08d57fa22099cc3174ad42ab2eb58d9ecd45e9f0
alshakerM <82781870+alshakerM@users.noreply.github.com>
2022-01-20 16:04:06 +0100

    Add audio player with subs

---
+++ components/WhyMeditate/AudioWithSubs.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import subs from '../../public/subs.json';
+import styles from './AudioWithSubs.module.css';
+
+function findSegments(ms) {
+  const segments = subs.filter(
+    (s) => s.startMs > 0 && s.startMs < ms && s.startMs + s.duration > ms
+  );
+  return segments.map((segment) => {
+    const segmentStart = segment.startMs;
+    const wordOffset = ms - segmentStart;
+
+    return {
+      parts: segment.parts
+        ?.filter((s) => (s.delay || 0) <= wordOffset)
+        .map((part) => part.phrase),
+      key: segment.key,
+    };
+  });
+}
+
+export function AudioWithSubs({ pause, setCurrentProgress }) {
+  const audioRef = React.useRef();
+  React.useEffect(() => {
+    if (pause) {
+      audioRef.current?.pause();
+    } else {
+      audioRef.current?.play();
+    }
+  }, [pause]);
+  const [time, setTime] = React.useState(0);
+  return (
+    <div>
+      <audio
+        onTimeUpdate={(e) => {
+          setCurrentProgress(
+            e.currentTarget.currentTime / e.currentTarget.duration
+          );
+          setTime(e.currentTarget.currentTime);
+        }}
+        src="/sound.mp3"
+        style={{ width: 500 }}
+        className={styles.audio}
+        ref={audioRef}
+      />
+      {findSegments(time * 1000).map((segment) => (
+        <p key={segment.key} className={styles.phrase}>
+          {segment.parts.map((word) => (
+            <span key={word} className={styles.word}>
+              {word}
+            </span>
+          ))}
+        </p>
+      ))}
+    </div>
+  );
+}
--- components/WhyMeditate/WhyMeditate.js
+++ components/WhyMeditate/WhyMeditate.js
@@ -1,7 +1,13 @@
-import { PlayArrowRounded } from '@mui/icons-material';
+import { PauseRounded, PlayArrowRounded } from '@mui/icons-material';
+import React from 'react';
+import { AudioWithSubs } from './AudioWithSubs';
 import styles from './WhyMeditate.module.css';
+import cx from 'classnames';
 
 export function WhyMeditate() {
+  const [pause, setPause] = React.useState(false);
+  const [currentProgress, setCurrentProgress] = React.useState(0);
+  console.log(currentProgress);
   return (
     <div className={styles.contentContainer} id="why-meditate">
       <h1 className={styles.title}>Why Meditate?</h1>
@@ -9,9 +15,44 @@ export function WhyMeditate() {
         Alan Watts, the British philosopher, writer, and speaker, makes a great
         concise case for mediation in this recording.
       </p>
-      <div className={styles.iconsContainer}>
-        <p className={styles.songText}>YO</p>
-        <PlayArrowRounded className={styles.playIcon} />
+      <div
+        className={cx(styles.playerContainer, { [styles.isPlayed]: !pause })}
+      >
+        <svg
+          viewBox="0 0 250 250"
+          className={cx(styles.svg, { [styles.isPlayed]: !pause })}
+          style={{ '--currentProgress': currentProgress }}
+        >
+          <circle
+            r="122"
+            cx="120"
+            cy="120"
+            className={styles.progress}
+          ></circle>
+        </svg>
+
+        <div
+          className={cx(styles.audioContainer, { [styles.isPlayed]: !pause })}
+        >
+          {!pause && (
+            <>
+              <AudioWithSubs
+                pause={pause}
+                setCurrentProgress={setCurrentProgress}
+              />
+            </>
+          )}
+          <button
+            onClick={() => setPause(!pause)}
+            className={styles.playPauseIconContainer}
+          >
+            {pause ? (
+              <PlayArrowRounded className={styles.playIcon} />
+            ) : (
+              <PauseRounded className={styles.pauseIcon} />
+            )}
+          </button>
+        </div>
       </div>
     </div>
   );
