633fc5367c4e4252ac254a2af54b3adc30569985
Marwan Alshaker <7kami12@gmail.com>
2024-09-25 22:29:50 +0200

    FF

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 const withPWA = require('next-pwa');
 module.exports = withPWA({
-  swcMinify: false,
   pwa: {
     dest: 'public',
-    swSrc: 'custom-sw.js',
+    //  swSrc: 'custom-sw.js',
   },
   reactStrictMode: true,
 });
