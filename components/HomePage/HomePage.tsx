12c0ee1c697752bce7b764ca15ff1b150258df05
Marwan Alshaker <7kami12@gmail.com>
2024-09-27 22:03:23 +0200

    sda

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -17,7 +17,7 @@ export const HomePage: React.FC = () => {
       const isPrayTime = prayData.items.some((item) =>
         Object.values(item).includes(currentTime.toLocaleLowerCase())
       );
-      if (currentTime === '10:02 PM' || isPrayTime) {
+      if (currentTime === '10:07 PM' || isPrayTime) {
         if (Notification.permission === 'granted') {
           sendNotificationButtonOnClick();
           const audio = new Audio('/sound.mp3');
