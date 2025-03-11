e46fb0b9a7196cc4ac696f6adc7514be42c4281c
Marwan Alshaker <7kami12@gmail.com>
2025-03-11 16:42:52 +0100

    Trigger build

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -10,6 +10,7 @@ import { useRouter } from 'next/router';
 
 export const HomePage: React.FC = () => {
   const [scrollY, setScrollY] = React.useState(0);
+
   const router = useRouter();
 
   React.useEffect(() => {
